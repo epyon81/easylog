294940e3e8b4b10a273a7e1697eeb047d8c32036
epyon <epyon81@users.noreply.github.com>
2016-07-29 18:42:48 +0200

    Allow interceptor annotation to be placed on methods.

--- easylog-jee/src/main/java/easylog/jee/Easylog.java
+++ easylog-jee/src/main/java/easylog/jee/Easylog.java
@@ -5,12 +5,13 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Inherited
 @InterceptorBinding
 @Retention(RUNTIME)
-@Target(TYPE)
+@Target({TYPE, METHOD})
 public @interface Easylog {
 }
